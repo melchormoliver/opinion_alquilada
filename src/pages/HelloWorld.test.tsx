47733716631d8abc3c6604bf5acb8a7a6ad060df
Mauro <torres.nicolas.mauro@gmail.com>
2021-01-03 00:20:55 -0300

    fix tests

--- src/pages/HelloWorld.test.tsx
+++ src/pages/HelloWorld.test.tsx
@@ -1,15 +1,10 @@
 import '../setupTests';
 import React from 'react';
 import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
 import HelloWorld from './HelloWorld';
 
 test('HelloWorld - renders without crashing', async () => {
   const { findByText } = render(<HelloWorld />);
-  await findByText('Hello World2');
-});
-
-test('should test Header component', () => {
-  const wrapper = shallow(<HelloWorld />);
-  expect(wrapper).toMatchSnapshot();
+  const result = await findByText('Hello World2');
+  expect(result).toBeDefined();
 });
