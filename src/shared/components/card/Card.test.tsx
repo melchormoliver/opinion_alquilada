a0dde4342a96c7fecdcbc3bf44a747d5b5f59683
Mauro <torres.nicolas.mauro@gmail.com>
2021-02-01 01:47:54 -0300

    fix name test

--- src/shared/components/card/Card.test.tsx
+++ src/shared/components/card/Card.test.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Card from './Card';
 
-test('HelloWorld - renders without crashing', async () => {
-  const { baseElement } = render(<Card />);
+test('Card - renders without crashing', async () => {
+  const { baseElement } = render(
+    <Card title='test' content='test' imgsrc='some path' />
+  );
   expect(baseElement).toBeDefined();
 });
